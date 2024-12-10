712760f12d98ea83600d103dc20a42624b531cba
Anjan Phuyal <anjanphuyal@Anjans-MacBook-Air.local>
2024-12-10 17:24:16 +0545

    completed all the section of management system and edited some bugs

--- src/Bankmanagementsystem/MiniStatement.java
+++ src/Bankmanagementsystem/MiniStatement.java
@@ -1,6 +1,7 @@
 package Bankmanagementsystem;
 
 import javax.swing.*;
+import java.awt.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -12,26 +13,51 @@ public class MiniStatement extends JFrame {
         setTitle("Mini Statement");
         setLayout(null);
 
-        JLabel text = new JLabel("");
-        add(text);
+        JLabel mini = new JLabel("");
+        add(mini);
 
-        JLabel bank = new JLabel("");
-        bank.setBounds(150,20,100,20);
+        JLabel bank = new JLabel("Nepal Rastra Bank");
+        bank.setFont(new Font("System",Font.BOLD,18));
+        bank.setBounds(110,30,200,20);
         add(bank);
 
         JLabel card = new JLabel("");
         card.setBounds(20,80,300,20);
         add(card);
 
+        JLabel balance = new JLabel("");
+        balance.setBounds(20,400,300,20);
+        add(balance);
+
         try{
             Conn conn = new Conn();
             ResultSet rs = conn.s.executeQuery("select * from login where pinnumber = '"+pinnumber+"'");
             while(rs.next()){
-                card.setText("Card Number: " + rs.getString("cardnumber"));
+                card.setText("Card Number: " + rs.getString("cardnumber").substring(0,4) + "XXXXXXX" + rs.getString("cardnumber").substring(12));
             }
         } catch (SQLException e) {
             System.out.println(e);
         }
+
+        try{
+            Conn conn = new Conn();
+            int bal = 0;
+            ResultSet rs = conn.s.executeQuery("select * from bank where pinnumber = '"+pinnumber+"'");
+            while(rs.next()){
+                mini.setText(mini.getText() + "<html>"+ rs.getString("date") + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + rs.getString("type") + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + rs.getString("amount") + "<br><br>" );
+                if (rs.getString("type").equals("Deposit")) {
+                    bal += Integer.parseInt(rs.getString("amount"));
+                } else {
+                    bal -= Integer.parseInt(rs.getString("amount"));
+                }
+            }
+
+            balance.setText("Your current account balance is Rs " + bal);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        mini.setBounds(20,140,400,200);
 
         setSize(400,600);
         setLocation(20,20);
